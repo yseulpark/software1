b50dfccbc01f4a9394a52148c65cd2815bb4383c
yseulpark <yseulpark26@gmail.com>
2026-01-15 11:49:23 +0200

    Module 2.py

---
+++ Module 2.py
@@ -0,0 +1,15 @@
+user = input('Enter your name: ')
+print("Hello, " + user + "!")
+
+import math
+
+radius = float(input("Enter your radius: "))
+area = math.pi * radius ** 2
+print("The area of the circle is " + str(area))
+
+length = float(input("Enter your length: "))
+width = float(input("Enter your width: "))
+perimeter = ( length + width ) *2
+print("The perimeter of the rectangle is " + str(perimeter))
+area = length * width
+print("The area of the rectangle is " + str(area))
