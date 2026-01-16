9cf263659c3ecbbc63767a1977afaba324690227
yseulpark <yseulpark26@gmail.com>
2026-01-16 22:00:54 +0200

    Module 2.6

---
+++ Module 2.6.py
@@ -0,0 +1,33 @@
+import random
+
+code_3 = ""
+no1 = random.randint(0,9)
+no2 = random.randint(0,9)
+no3 = random.randint(0,9)
+
+no1 = str(no1)
+no2 = str(no2)
+no3 = str(no3)
+
+code_3 += no1
+code_3 += no2
+code_3 += no3
+print(code_3)
+
+code_4 = ""
+no4 = random.randint(1,6)
+no5 = random.randint(1,6)
+no6 = random.randint(1,6)
+no7 = random.randint(1,6)
+
+no4 = str(no4)
+no5 = str(no5)
+no6 = str(no6)
+no7 = str(no7)
+
+code_4 += no4
+code_4 += no5
+code_4 += no6
+code_4 += no7
+print(code_4)
+
