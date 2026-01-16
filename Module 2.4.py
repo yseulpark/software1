1f750972aed7e7aa2ca70b2d92e0c8fdd85faeb8
yseulpark <yseulpark26@gmail.com>
2026-01-16 20:18:03 +0200

    Module 2.4.py

---
+++ Module 2.4.py
@@ -0,0 +1,11 @@
+no1 = int(input("Enter the first number : "))
+no2 = int(input("Enter the second number : "))
+no3 = int(input("Enter the third number : "))
+sum = no1 + no2 + no3
+product = no1 * no2 * no3
+average = sum / 3
+print("The sum of the numbers is", sum)
+print("The product of the numbers is", product)
+print("The average of the numbers is", average)
+
+
