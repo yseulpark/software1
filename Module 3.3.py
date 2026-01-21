39fd2bcb3074d0d8cd82c38e36a9cb594dca3493
yseulpark <yseulpark26@gmail.com>
2026-01-21 21:17:23 +0200

    Module 3.3.py

--- Module 3.3.py
+++ Module 3.3.py
@@ -1,16 +1,18 @@
 gender = input("Enter your biological gender: ")
-hemoglobin_value = float(input("Enter your hemoglobin value: "))
+hemoglobin_value = float(input("Enter your hemoglobin value (g/l): "))
 
-if gender == "female" and 117 < hemoglobin_value < 155 :
-    print ("Normal hemoglobin value")
-elif gender == "female" and hemoglobin_value >= 155 :
-    print ("High hemoglobin value")
-elif gender == "female" and hemoglobin_value <= 117 :
-    print ("Low hemoglobin value")
+if gender == "female":
+    if 117 < hemoglobin_value < 155:
+        print ("Normal hemoglobin value")
+    elif hemoglobin_value >= 155 :
+        print ("High hemoglobin value")
+    elif hemoglobin_value <= 117 :
+        print ("Low hemoglobin value")
 
-if gender == "male" and 134 < hemoglobin_value < 167 :
-    print ("Normal hemoglobin value")
-elif gender == "male" and hemoglobin_value >= 167 :
-    print("High hemoglobin value")
-elif gender == "male" and hemoglobin_value <= 134:
-    print("Low hemoglobin value")
+if gender == "male":
+    if 134 < hemoglobin_value < 167:
+        print ("Normal hemoglobin value")
+    elif hemoglobin_value >= 167 :
+        print ("High hemoglobin value")
+    elif hemoglobin_value <= 134 :
+        print ("Low hemoglobin value")
