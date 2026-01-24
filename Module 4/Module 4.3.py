9b4966c418a9a770026b1809f98d4d0eca900e9e
yseulpark <yseulpark26@gmail.com>
2026-01-24 20:57:19 +0200

    Module 4.3

--- Module 4/Module 4.3.py
+++ Module 4/Module 4.3.py
@@ -1,10 +1,11 @@
 number = str(input("Enter the number:"))
-max = min = float(number)
-while number != "":
-    number = float(number)
-    if number > max:
-        max = number
-    elif number < min:
-        min = number
-    number = str(input("Enter the number:"))
-print (f"The largest number is {max} and the smallest number is {min}.")
+if number !="":
+    max = min = float(number)
+    while number != "":
+        number = float(number)
+        if number > max:
+            max = number
+        elif number < min:
+            min = number
+        number = str(input("Enter the number:"))
+    print (f"The largest number is {max} and the smallest number is {min}.")
