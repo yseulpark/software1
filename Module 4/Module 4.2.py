f6a5155499b10dbc3714bab45e45bb028cf7fb64
yseulpark <yseulpark26@gmail.com>
2026-01-24 20:21:59 +0200

    Module 4.2

---
+++ Module 4/Module 4.2.py
@@ -0,0 +1,5 @@
+inches = float(input("Enter the number of inches: "))
+while inches >= 0:
+    centimeters = inches * 2.54
+    print (centimeters)
+    inches = float(input("Enter the number of inches: "))
