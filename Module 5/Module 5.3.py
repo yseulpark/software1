ee2f67dacd05d2c05952c2a70db9471f3c55a0ed
yseulpark <yseulpark26@gmail.com>
2026-01-31 22:03:45 +0200

    Module 5.3

---
+++ Module 5/Module 5.3.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number: "))
+is_prime = True
+
+for i in range (2,number):
+    if number % i == 0 :
+        is_prime = False
+        break
+if is_prime == True :
+    print (f"{number} is a prime number.")
+else :
+    print (f"{number} is not a prime number.")
