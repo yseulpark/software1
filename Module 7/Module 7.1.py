8f3704be189c69d608a8cd7e518b31e8d8e76b64
yseulpark <yseulpark26@gmail.com>
2026-02-03 19:57:42 +0200

    Module 7.1

--- Module 7/Module 7.1.py
+++ Module 7/Module 7.1.py
@@ -1,11 +1,14 @@
-seasons = ("Spring", "Summer", "Autumn", "Winter")
-month =  int(input("Enter the month number (1-12): "))
+seasons = ("Spring","Summer","Autumn","Winter")
+print ("1-January\n2-February\n3-March\n4-April\n5-May\n6-June\n7-July\n8-August\n9-September\n10-October\n11-November\n12-December")
+user_input = int(input("Choose a month by entering the corresponding number:"))
 
-if month == 12 or month ==1 or month == 2:
-    print (seasons[3])
-elif month == 3 or month == 4 or month == 5:
-    print (seasons[0])
-elif month == 6 or month == 7 or month == 8:
-    print (seasons[1])
-elif month == 9 or month == 10 or month == 11:
-    print (seasons[2])
+if user_input == 12 or user_input == 1 or user_input == 2:
+    print (f"Month number {user_input} is in {seasons[3]}.")
+elif user_input == 3 or user_input == 4 or user_input == 5:
+    print (f"Month number {user_input} is in {seasons[0]}.")
+elif user_input == 6 or user_input == 7 or user_input == 8:
+    print (f"Month number {user_input} is in {seasons[1]}.")
+elif user_input == 9 or user_input == 10 or user_input == 11:
+    print (f"Month number {user_input} is in {seasons[2]}.")
+else :
+    print ("Invalid input.")
